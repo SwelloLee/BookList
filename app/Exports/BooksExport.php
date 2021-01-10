389c2068dac6e06a5ad391e5ce6fad2bc5487ae5
Swello Lee <swellolee@gmail.com>
2021-01-11 00:33:55 +0800

    Update BooksExport.php
    Changed the export to only include the title and author columns

--- app/Exports/BooksExport.php
+++ app/Exports/BooksExport.php
@@ -4,14 +4,17 @@ namespace App\Exports;
 
 use App\Models\Book;
 use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\FromQuery;
+use Maatwebsite\Excel\Concerns\Exportable;
 
-class BooksExport implements FromCollection
+class BooksExport implements FromQuery
 {
+    use Exportable;
     /**
     * @return \Illuminate\Support\Collection
     */
-    public function collection()
+    public function query()
     {
-        return Book::all();
+        return Book::query()->select('title','author');
     }
 }
